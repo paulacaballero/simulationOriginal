75996ef9e61e78afab11b93d04691b26aebd893d
paulacaballero <paula.caballero@alumni.mondragon.edu>
2024-12-09 10:59:14 +0100

    with semaphores

--- src/main/java/simulator/Doctor.java
+++ src/main/java/simulator/Doctor.java
@@ -1,27 +1,24 @@
 package simulator;
 
-public class Doctor extends Thread {
 
+public class Doctor extends Thread {
+    
     private int specialty;
     private WaitingRoom waitingRoom;
 
     public Doctor(WaitingRoom waitingRoom, int specialty, int id) {
         super("Doctor " + id);
-        
         this.specialty = specialty;
         this.waitingRoom = waitingRoom;
     }
-    
 
     public int getSpecialty() {
         return specialty;
     }
 
-
     public void setSpecialty(int specialty) {
         this.specialty = specialty;
     }
-
 
     @Override
     public void run() {
--- src/main/java/simulator/Patient.java
+++ src/main/java/simulator/Patient.java
@@ -4,7 +4,6 @@ public class Patient extends Thread {
 
     private WaitingRoom waitingRoom;
     private int specialty;
-    private double priority;
 
     public Patient(WaitingRoom waitingRoom, int id) {
         super("Patient " + id);
@@ -19,17 +18,6 @@ public class Patient extends Thread {
     public void setSpecialty(int specialty) {
         this.specialty = specialty;
     }
-
-
-    public double isPriority() {
-        return priority;
-    }
-
-
-    public void setPriority(double priority) {
-        this.priority = priority;
-    }
-
 
     @Override
     public void run() {
