4c1ad62c8e432808e22de46e5e25ffc3007b1cfd
Asier21 <asier.iglesiasa@alumni.mondragon.edu>
2025-01-20 08:43:25 +0100

    Materials for doctors

--- src/main/java/simulator/Doctor.java
+++ src/main/java/simulator/Doctor.java
@@ -1,15 +1,20 @@
 package simulator;
 
+import java.util.concurrent.Semaphore;
 
 public class Doctor extends Thread {
-    
+
     private int specialty;
     private WaitingRoom waitingRoom;
+    private int materialCount;
+    private static final int MAX_MATERIAL = 3;
+    private static Semaphore supplyStation = new Semaphore(2);
 
     public Doctor(WaitingRoom waitingRoom, int specialty, int id) {
         super("Doctor " + id);
         this.specialty = specialty;
         this.waitingRoom = waitingRoom;
+        this.materialCount = MAX_MATERIAL; 
     }
 
     public int getSpecialty() {
@@ -24,11 +29,25 @@ public class Doctor extends Thread {
     public void run() {
         while (!isInterrupted()) {
             try {
+                if (materialCount == 0) {
+                    restockMaterials();
+                }
                 // They attend patients in their queues
                 waitingRoom.attend(this);
+                materialCount--;
             } catch (InterruptedException e) {
                 interrupt();
             }
         }
+    }
+
+    private void restockMaterials() throws InterruptedException {
+        System.out.println(getName() + " is out of materials and needs to restock.");
+        supplyStation.acquire(); 
+        System.out.println(getName() + " is restocking materials.");
+        Thread.sleep(2000);
+        materialCount = MAX_MATERIAL; 
+        System.out.println(getName() + " has restocked materials and can treat more patients.");
+        supplyStation.release(); 
     }
 }
