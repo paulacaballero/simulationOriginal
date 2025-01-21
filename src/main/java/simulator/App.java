51e2fd6d53df01a8af95fb8aca93d6ff33d0fd84
Aritz <ariestevev@gmail.com>
2025-01-21 09:33:34 +0100

    ready for testing

--- src/main/java/simulator/App.java
+++ src/main/java/simulator/App.java
@@ -132,7 +132,7 @@ public void calculateAvgWaitingTime() {
         }
     }
     public static void main(String[] args) {
-        for(int i =0; i<1;i++){
+        for(int i =0; i<10;i++){
             App app = new App();
 
             app.startThreads();
